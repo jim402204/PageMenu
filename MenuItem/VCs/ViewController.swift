3e21792f4534441698122d198cf6ae7b04155d61
tim <tim_chiang@zonvan.com.tw>
2020-10-26 12:34:51 +0800

    get collectionView Item content offset update menu Slider bar loaction

--- MenuItem/VCs/ViewController.swift
+++ MenuItem/VCs/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 //https://github.com/rechsteiner/Parchment
 //https://github.com/yysskk/SwipeMenuViewController
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var menuView: UICollectionView!
     
     var leftConstraint: NSLayoutConstraint? = nil
-    let models = [Int](1...10)
+    let menuModeld = ["芭蕉","旦蕉","金鑽鳳梨","鳳梨花","蓬萊仙山999","甜蜜蜜","檸檬","牛奶鳳梨","葡萄柚","柚子"]
     
     lazy var pageVC: PageVC? = { return self.children.first as? PageVC }()
     
@@ -33,8 +34,12 @@ class ViewController: UIViewController {
         pageVC?.scrollView?.delegate = self
     }
     
-    
+}
 
+// MARK: setUpView
+
+extension ViewController {
+    
     func setUpCollectionView() {
     
         menuView.delegate = self
@@ -46,6 +51,7 @@ class ViewController: UIViewController {
         
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        //如果要浮動item size 需要覆寫 layotut attribute 方法
         layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 4, height: 40)
         layout.minimumLineSpacing = 0.3
         layout.scrollDirection = .horizontal
@@ -71,18 +77,20 @@ class ViewController: UIViewController {
 }
 
 
+// MARK: UICollectionView
+
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return models.count
+        return menuModeld.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let item = menuView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LabelCollectionViewCell
         
-        let text = models[indexPath.row]
-        item.title.text = "title" + String(text)
+        let text = menuModeld[indexPath.row]
+        item.title.text = String(text)
         item.title.textColor = .black
         
         
@@ -93,14 +101,20 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
         collectionView.selectItem(at: IndexPath(row: indexPath.row, section: 0), animated: true, scrollPosition: .centeredHorizontally)
         
-        //字一樣大才有這個效果   //不一樣大要取得各個item左邊的點在contentOffset的位置
-        let result = (collectionView.frame.width / 4.0) * CGFloat(indexPath.row)
-        self.leftConstraint?.constant = result
+        //直接取得 item對應的contentoffset 的frame
+        let itemAttributes = self.menuView.collectionViewLayout.layoutAttributesForItem(at: indexPath) //item object
+        print("itemAttributes: \(String(describing: itemAttributes?.frame.minX))")
+
+        let defaultWidth = getItemContentOffsetWidth(collectionView: collectionView, indexPath)
+        let itemContentOffset = itemAttributes?.frame.minX ?? defaultWidth
+        
+        self.leftConstraint?.constant = itemContentOffset
+        
         
         updateMenuViewForMenuSiderAnimate()
         
         // 儲存的位置 供滑動基準。  儲存index 供選擇動畫使用
-        self.currentLoaction = result
+        self.currentLoaction = itemContentOffset
         self.currentRow = indexPath.row
     }
     
@@ -111,11 +125,23 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         } completion: { bool in}
     }
     
+    func getItemContentOffsetWidth(collectionView: UICollectionView, _ indexPath: IndexPath) -> CGFloat {
+        //字一樣大才有這個效果   //不一樣大要取得各個item左邊的點在contentOffset的位置
+        let result = (collectionView.frame.width / 4.0) * CGFloat(indexPath.row)
+        return result
+    }
+    
 }
+
+// MARK: UIScrollView
 
 extension ViewController: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        
+        let screenWidth = UIScreen.main.bounds.width
+        
         
         guard !(scrollView is UICollectionView) else { return }
     
